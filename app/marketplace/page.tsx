87523ce2780589c9bf512c3df74975a973a02775
wis12dom34 <faceb8841@gmail.com>
2026-09-04 09:52:41 +0100

    Add Marketplace screen

---
+++ app/marketplace/page.tsx
@@ -0,0 +1,2 @@
+import { PageShell } from "@/components/PageShell";
+export default function Marketplace(){return <PageShell title="Marketplace" subtitle="Digital products"><div className="panel"><h3>Coming soon</h3><p>Marketplace purchasing will be enabled only after the verified FADDED provider order contract is connected. No provider endpoint is guessed here.</p></div></PageShell>}
