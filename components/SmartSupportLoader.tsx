fb4ef3c7f1e52bdaf1103f6ed0344bf9a32402b5
wis12dom34 <faceb8841@gmail.com>
2026-09-07 21:02:39 +0100

    Mount contextual Smart Support help

--- components/SmartSupportLoader.tsx
+++ components/SmartSupportLoader.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from "next/dynamic";
 import {SmartSupportDashboardPortal} from "@/components/SmartSupportDashboardPortal";
+import {SmartSupportContextEntry} from "@/components/SmartSupportContextEntry";
 
 const SmartSupport=dynamic(()=>import("@/components/SmartSupport"),{ssr:false,loading:()=>null});
-export function SmartSupportLoader(){return <><SmartSupport/><SmartSupportDashboardPortal/></>}
+export function SmartSupportLoader(){return <><SmartSupport/><SmartSupportDashboardPortal/><SmartSupportContextEntry/></>}
