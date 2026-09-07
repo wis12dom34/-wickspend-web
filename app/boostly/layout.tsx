0eb7d9a5690143d9f65838ac518dc3384145cfbe
wis12dom34 <faceb8841@gmail.com>
2026-09-07 19:47:59 +0100

    Load Boostly blue theme overrides

--- app/boostly/layout.tsx
+++ app/boostly/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import BoostlyDeepLink from "./BoostlyDeepLink";
+import "./boostly-theme.css";
 
 export default function BoostlyLayout({ children }: { children: ReactNode }) {
-  return <><BoostlyDeepLink />{children}</>;
+  return <div className="boostlyTheme"><BoostlyDeepLink />{children}</div>;
 }
