40a4aaca6293b6f7ea2bea8c8e738f127d66fa8c
wis12dom34 <faceb8841@gmail.com>
2026-09-08 03:39:00 +0100

    Remove Smart Support quick action from dashboard

--- components/SmartSupportLoader.tsx
+++ components/SmartSupportLoader.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import {SmartSupportDashboardPortal} from "@/components/SmartSupportDashboardPortal";
 import {SmartSupportContextEntry} from "@/components/SmartSupportContextEntry";
 
 const SmartSupport=dynamic(()=>import("@/components/SmartSupport"),{ssr:false,loading:()=>null});
-export function SmartSupportLoader(){return <><SmartSupport/><SmartSupportDashboardPortal/><SmartSupportContextEntry/></>}
+export function SmartSupportLoader(){return <><SmartSupport/><SmartSupportContextEntry/></>}
