3c09f1040e4278b3b0b52f01722013679a02d73d
wis12dom34 <faceb8841@gmail.com>
2026-09-08 07:05:06 +0100

    Restore WickSpend Smart Support and hide Smartsupp chat

--- components/SmartSupportLoader.tsx
+++ components/SmartSupportLoader.tsx
@@ -1,33 +1,10 @@
 "use client";
 
-import {useEffect} from "react";
+import dynamic from "next/dynamic";
+import {SmartSupportContextEntry} from "@/components/SmartSupportContextEntry";
 
-declare global {
-  interface Window {
-    _smartsupp?: {key?: string};
-    smartsupp?: any;
-  }
-}
-
-const SMARTSUPP_KEY="674ab7d0fa7495b8b776dcbd3d2ea12bb94cbce8";
-const SMARTSUPP_SCRIPT_ID="wickspend-smartsupp-loader";
+const SmartSupport=dynamic(()=>import("@/components/SmartSupport"),{ssr:false,loading:()=>null});
 
 export function SmartSupportLoader(){
-  useEffect(()=>{
-    if(typeof window==="undefined"||typeof document==="undefined")return;
-    window._smartsupp=window._smartsupp||{};
-    window._smartsupp.key=SMARTSUPP_KEY;
-    if(window.smartsupp||document.getElementById(SMARTSUPP_SCRIPT_ID))return;
-    const queue:any=function(...args:any[]){queue._.push(args)};
-    queue._=[];
-    window.smartsupp=queue;
-    const script=document.createElement("script");
-    script.id=SMARTSUPP_SCRIPT_ID;
-    script.type="text/javascript";
-    script.charset="utf-8";
-    script.async=true;
-    script.src="https://www.smartsuppchat.com/loader.js?";
-    document.head.appendChild(script);
-  },[]);
-  return null;
+  return <><SmartSupport/><SmartSupportContextEntry/></>;
 }
